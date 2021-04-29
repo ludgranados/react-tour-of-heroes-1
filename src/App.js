36fa1338ab15de52a1451ce08dc142a2f44932d5
Ludwin Granados <ludwingranados@gmail.com>
2021-04-29 14:33:15 -0700

    Refactor: Wrapp App with GlobalProvider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { GlobalProvider } from './context/GlobalContext';
 import AppRouter from './routers/AppRouter';
 
 const App = () => {
   return (
-    <Router>
+    <GlobalProvider>
       <AppRouter />
-    </Router>
+    </GlobalProvider>
   );
 };
 
